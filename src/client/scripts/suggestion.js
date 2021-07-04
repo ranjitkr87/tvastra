1a0da05be64df82362395311d0c62589c8fb826f
Ranjit <ranjitkr87@gmail.com>
2021-07-04 11:33:46 +0530

    index update

---
+++ src/client/scripts/suggestion.js
@@ -0,0 +1,15 @@
+const doctors=[ {
+    name: "Charlie Moon"
+  },
+  {
+    name: "Coni Star"
+  },
+  {
+    name: "Kisten Misel"
+  },
+  {
+    name: "John Nission"
+  },
+  {
+    name: "Rose Mooon"
+  }];
