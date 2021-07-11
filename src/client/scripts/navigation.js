d0617a1dd51819decbdfc763ee0854af7887b570
Ranjit <ranjitkr87@gmail.com>
2021-07-11 16:16:01 +0530

    lohin signup limks

--- src/client/scripts/navigation.js
+++ src/client/scripts/navigation.js
@@ -3,9 +3,3 @@ var loginDirection=document.getElementsByClassName("loginDirect")[0];
 loginDirection.addEventListener("click", function(e) {
     window.open("../views/login.html"); 
 }, false);
-
-var SignUpDirection=document.getElementsByClassName("signUpDirect")[0];
-
-SignUpDirection.addEventListener("click", function(e) {
-    window.open("../views/signup.html"); 
-}, false);
