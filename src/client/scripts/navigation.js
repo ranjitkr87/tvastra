0784884ddf1b889a2004bc8846e20ad1aa1f77fb
Ranjit <ranjitkr87@gmail.com>
2021-09-06 16:24:15 +0530

    success failure timeout updated

---
+++ src/client/scripts/navigation.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+    document.getElementById('success').style.display = 'none';
+}, 4000);
+
+setTimeout(function(){
+    document.getElementById('failure').style.display = 'none';
+}, 4000);
